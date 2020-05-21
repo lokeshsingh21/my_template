8a2f44a6eb2168f34af37202b827f7edd8e9e810
Lokesh Singh <lokeshsingh2182000@gmail.com>
2020-05-21 17:38:50 +0530

    Update lokeshsinghc++template.cpp

--- lokeshsinghc++template.cpp
+++ lokeshsinghc++template.cpp
@@ -24,21 +24,30 @@ template<class T> using oset=tree<T,null_type,less<T>,rb_tree_tag,tree_order_sta
 #define     repb(i,b,a) for(int i=int(b);i>=int(a);i--)
 #define     FastIO      ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
 
-struct custom_hash {        
-    // unordered_map<int,int,custom_hash>
+struct custom_hash {        // unordered_map<int,int,custom_hash>
     static uint64_t splitmix64(uint64_t x) {
         x += 0x9e3779b97f4a7c15;
         x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
         x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
         return x ^ (x >> 31);
     }
-
     size_t operator()(uint64_t x) const {
         static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
         return splitmix64(x + FIXED_RANDOM);
     }
 };
+
+struct comp{            // custom compare function
+    bool operator()(pii a,pii b){
+
+    }
+};
+
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int getrand(int a,int b){
+    uniform_int_distribution<int> uid(a,b);
+    return uid(rng);
+}
 
 typedef double db;
 typedef long long ll;
